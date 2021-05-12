8dab135a8ef5df4d33cd651dcff4d61d4ffe543a
yukki373 <78712648+yukki373@users.noreply.github.com>
2021-05-13 02:05:52 +0900

    ⌘ + Enter で送信できるようにする

--- app/javascript/channels/chat_room_channel.js
+++ app/javascript/channels/chat_room_channel.js
@@ -21,7 +21,7 @@ const appChatRoom = consumer.subscriptions.create("ChatRoomChannel", {
 
 if(/chat_rooms/.test(location.pathname)) {
   $(document).on("keydown", ".chat-room__message-form_textarea", function(e) {
-    if (e.key === "Enter") {
+    if (e.key === "Enter" && e.metaKey) {
       const chat_room_id = $('textarea').data('chat_room_id')
       appChatRoom.speak(e.target.value,chat_room_id);
       e.target.value = '';
